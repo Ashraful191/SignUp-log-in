daf78cd22f62c46db6bda3cd002bddb3e533efce
Ashraful Islam <115151345+Ashraful191@users.noreply.github.com>
2023-01-18 18:18:16 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const form =  document.querySelector("form");
+const name = document.querySelector("div #name");
+const email = document.querySelector("div #email");
+const password = document.querySelector("div #password");
+
+
+form.addEventListener('submit', formHandler);
+
+function formHandler(e){
+    e.preventDefault();
+    
+
+
+const userInfo = {
+    name :name.value,
+    email: email.value,
+    password: password.value,
+};
+
+console.log(userInfo);
+name.value = "";
+email.value = "";
+password.value = "";
+
+}
